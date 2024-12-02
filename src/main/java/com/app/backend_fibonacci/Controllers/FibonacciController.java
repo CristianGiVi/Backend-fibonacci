1f144d0fb0f6e8ba1dc19274c5709fec310a3f2a
Cristian Giraldo <cgiraldo@unal.edu.co>
2024-12-02 16:12:32 -0500

    Implementacion Swagger

--- src/main/java/com/app/backend_fibonacci/Controllers/FibonacciController.java
+++ src/main/java/com/app/backend_fibonacci/Controllers/FibonacciController.java
@@ -3,6 +3,12 @@ package com.app.backend_fibonacci.Controllers;
 
 import com.app.backend_fibonacci.Models.Fibonacci;
 import com.app.backend_fibonacci.Services.FibonacciService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +27,61 @@ public class FibonacciController {
     private FibonacciService fibonacciService;
 
     @GetMapping
+    @Operation(
+        summary = "Obtiene la lista de series de Fibonacci generadas",
+        description = "Devuelve una lista de objetos Fibonacci previamente generados y almacenados en la base de" +
+            " datos mysql."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Lista obtenida correctamente",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Fibonacci.class),
+                examples = @ExampleObject(
+                    value = """
+                        [
+                        {
+                            "id": 1,
+                            "fibonacciSerie": "[53243, 32906, 20337, 12569, 7768, 4801, 2967, 1834, 1133, 701, 432, 269,
+                             163, 106, 57, 49, 8]",
+                            "time": "15:49:08"
+                        }
+                        ]
+                    """
+                )
+            )
+        )
+    })
     public List<Fibonacci> list(){
         return fibonacciService.findAll();
     }
 
+
     @GetMapping("/current-time")
+    @Operation(
+        summary = "Genera una serie de Fibonacci con la hora actual",
+        description = "Calcula una serie de Fibonacci usando la hora actual del sistema."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "201",
+            description = "Serie de Fibonacci generada correctamente",
+            content = @Content(
+                mediaType = "application/json",
+                examples = @ExampleObject(value = "{ \"success\": true, \"fibonacciSerie\": \"52, 32, 20, 12, 8\" }")
+            )
+        ),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Error interno del servidor",
+            content = @Content(
+                mediaType = "application/json",
+                examples = @ExampleObject(value = "{ \"success\": false, \"message\": \"Error interno del servidor\" }")
+            )
+        )
+    })
     public ResponseEntity<?> generateFibonacci(){
         try{
             String fibonacciSerie = fibonacciService.generateSerie();
@@ -50,7 +106,44 @@ public class FibonacciController {
     */
 
     @PostMapping("/time")
-    public ResponseEntity<?> generateFibonacciWithTime(@RequestBody Map<String, String> requestBody){
+    @Operation(
+        summary = "Genera una serie de Fibonacci basada en un tiempo específico",
+        description = "Calcula una serie de Fibonacci usando un valor de tiempo " +
+            "proporcionado en el cuerpo de la solicitud."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "201",
+            description = "Serie de Fibonacci generada correctamente",
+            content = @Content(
+                mediaType = "application/json",
+                examples = @ExampleObject(value = "{ \"success\": true, \"fibonacciSerie\": \"52, 32, 20, 12, 8\" }")
+            )
+        ),
+    @ApiResponse(
+        responseCode = "400",
+        description = "Error en los datos de entrada",
+        content = @Content(
+            mediaType = "application/json",
+            examples = @ExampleObject(value = "{ \"success\": false, \"message\": \"Hora no válida.\" }")
+        )
+    ),
+    @ApiResponse(
+        responseCode = "500",
+        description = "Error interno del servidor",
+        content = @Content(
+            mediaType = "application/json",
+            examples = @ExampleObject(value = "{ \"success\": false, \"message\": \"Error interno del servidor\" }")
+        )
+    )})
+    public ResponseEntity<?> generateFibonacciWithTime(
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        description = "Tiempo en formato HH:mm:ss para generar la serie de Fibonacci.",
+        content = @Content(
+            mediaType = "application/json",
+            examples = @ExampleObject(value = "{ \"time\": \"15:49:08\" }")
+        )
+        )@RequestBody Map<String, String> requestBody){
         try {
             String timeString = requestBody.get("time");
             String fibonacciSerie = fibonacciService.generateSerieWithSelectedTime(timeString);
